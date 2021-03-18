6ad4fe001432d21913f818bfd5fe1ddad4f5d509
-- <mixool0204@gmail.com>
2021-03-18 21:35:42 +0800

    支持网络配置

--- jdformat.sh
+++ jdformat.sh
@@ -11,39 +11,43 @@ workdir="/jd_sku/jd_scripts"
 cookiefile="$workdir/cookie.file"
 composefile="$workdir/docker-compose.yml"
 
-# 基础工具和文件创建
 function jd_sku_base(){
-    [[ ! -d "$workdir" ]] && mkdir -p $workdir || { workdir_bak=${workdir}_$(date +%s); mv $workdir $workdir_bak; echo $workdir 文件已备份至 $workdir_bak; }
+    # 文件目录
+    [[ ! -d "$workdir" ]] && mkdir -p $workdir || { workdir_bak=${workdir}_$(date +%Y_%m_%d_%H_%M); mv $workdir $workdir_bak; echo $workdir 文件已备份至 $workdir_bak; }
     git clone https://github.com/mixool/jd_sku.git $workdir
 }
 
-# docker-compose.yml 所有支持的变量： jd_sku_var@ENABLE_AUTO_HELP@true jd_sku_var@TG_BOT_TOKEN@123456:AABB jd_sku_var@TG_USER_ID@-123456
 function jd_sku_var(){
-    varlist=($(echo ${all_parameter[*]} | grep -oE "jd_sku_var@[^@]+@[^ ]+" | tr "\n" " ")) && [[ ${#varlist[*]} == 0 ]] && return 0
+    # 网络地址下载docker-compose.yml配置 jd_sku_var@https://gist.github.com 
+    compose_file_url=$(echo ${all_parameter[*]} | grep -oE "jd_sku_var@http[^ ]+" | cut -f2 -d@ | head -n1)
+    [[ $compose_file_url != "" ]] && wget -O $composefile $compose_file_url && return 0
+    # 传入参数获取docker-compose.yml配置 jd_sku_var@ENABLE_AUTO_HELP@true jd_sku_var@TG_BOT_TOKEN@123456:AABB jd_sku_var@TG_USER_ID@-123456 
+    varlist=($(echo ${all_parameter[*]} | grep -oE "jd_sku_var@[^@]+@[^ ]+" | tr "\n" " "))
     for ((i = 0; i < ${#varlist[*]}; i++)); do
         varname="$(echo ${varlist[i]} | cut -f2 -d@)"
         varvalue="$(echo ${varlist[i]} | cut -f3 -d@)"
         [[ $i == 0 ]] && echo >>$composefile
-        [[ $varname == "" || $varvalue == "" ]] && echo 变量 $varname $varvalue 参数无效 && continue
+        [[ $varname == "" || $varvalue == "" ]] && continue
         sed -i "/$varname.*/d" $composefile
         echo "                - $varname=$varvalue" >>$composefile
     done
-    cat $composefile
 }
 
-# 从workdir目录下的cookie.file获取格式化后cookie导入docker-compose.yml,后期cookie过期维护使用: bash jdformat.sh jd_sku_initck
 function jd_sku_initck() {
-    cookies="$(cat $cookiefile | grep -vE "^#" | tr "\n" "&" | sed "s/&$//")"
+    # 传入参数格式为 jd_sku_initck@https://gist.github.com 时从网络地址获取cookie,格式为jd_sku_initck时从workdir目录下的cookie.file获取cookie，再配置docker-compose.yml
+    cookie_file_url=$(echo ${all_parameter[*]} | grep -oE "jd_sku_initck@http[^ ]+" | cut -f2 -d@ | head -n1)
+    [[ $cookie_file_url != "" ]] && wget -O $cookiefile $cookie_file_url && cookies="$(cat $cookiefile | grep -vE "^#" | tr "\n" "&" | sed "s/&$//")"
+    [[ $cookie_file_url == "" ]] && cookies="$(cat $cookiefile | grep -vE "^#" | tr "\n" "&" | sed "s/&$//")"
     sed -i "/JD_COOKIE.*/d" $composefile
     echo "                - JD_COOKIE=$cookies" >>$composefile
-    cat $composefile
     return 0
 }
 
 function main() {
-    echo ${all_parameter[*]} | grep -q "jd_sku_initck" && jd_sku_initck && exit 0
     jd_sku_base
     jd_sku_var
+    jd_sku_initck
+    cat $composefile
 }
 
 main
