9a26ca5944eaf9b8a94ea645919935b438fd2cb8
-- <mixool0204@gmail.com>
2021-03-30 11:02:58 +0800

    monkcoder

--- jd_diy.sh
+++ jd_diy.sh
@@ -1,12 +1,48 @@
 #!/usr/bin/env bash
 ## CUSTOM_SHELL_FILE for https://gitee.com/lxk0301/jd_docker/tree/master/docker
 
-#### i-chenzhe https://github.com/i-chenzhe/qx
-rm -rf /i-chenzhe
-git clone https://github.com/i-chenzhe/qx.git /i-chenzhe
-#
-for jsname in $(ls /i-chenzhe | grep -E "js$" | tr "\n" " "); do cp /i-chenzhe/$jsname /scripts/i-chenzhe_$jsname; done
-for jsname in $(ls /i-chenzhe | grep -E "js$" | tr "\n" " "); do
-    jsnamecron="$(cat /i-chenzhe/$jsname | grep -A 1 -E "\[Script\]" | grep -oE "^cron.*$jsname" | cut -d\" -f2)"
-    test -z "$jsnamecron" || echo "$jsnamecron node /scripts/i-chenzhe_$jsname >> /scripts/logs/i-chenzhe_$jsname.log 2>&1" >> /scripts/docker/merged_list_file.sh
-done
+function monkcoder(){
+    # https://github.com/monk-coder/dust
+    rm -rf /monkcoder
+    git clone https://github.com/monk-coder/dust.git /monkcoder
+    # 拷贝新文件
+    for jsname in $(find /monkcoder -name "*.js" | grep -vE "\/backup\/"); do cp ${jsname} /scripts/monkcoder_${jsname##*/}; done
+    for jsname in $(find /monkcoder -name "*.js" | grep -vE "\/backup\/"); do
+        jsnamecron="$(cat $jsname | grep -oE "/?/?cron \".*\"" | cut -d\" -f2)"
+        test -z "$jsnamecron" || echo "$jsnamecron node /scripts/monkcoder_${jsname##*/} >> /scripts/logs/monkcoder_${jsname##*/}.log 2>&1" >> /scripts/docker/merged_list_file.sh
+    done
+}
+
+function whyour(){
+    # https://github.com/whyour/hundun/tree/master/quanx
+    rm -rf /whyour
+    git clone https://github.com/whyour/hundun.git /whyour
+    # 拷贝新文件
+    for jsname in jx_factory.js jx_factory_component.js jdzz.js jd_zjd_tuan.js; do cp /whyour/quanx/$jsname /scripts/whyour_$jsname; done
+    for jsname in jx_factory.js jx_factory_component.js jdzz.js jd_zjd_tuan.js; do
+        jsnamecron="$(cat /whyour/$jsname | grep -oE "/?/?cron \".*\"" | cut -d\" -f2)"
+        test -z "$jsnamecron" || echo "$jsnamecron node /scripts/whyour_$jsname >> /scripts/logs/whyour_$jsname.log 2>&1" >> /scripts/docker/merged_list_file.sh
+    done
+}
+
+function diycron(){
+    # 启用京价保
+    echo "41 0,23 * * * node /scripts/jd_price.js >> /scripts/logs/jd_price.log 2>&1" >> /scripts/docker/merged_list_file.sh
+}
+
+function main(){
+    #
+    a_jsnum=$(ls -l /scripts | grep -oE "^-.*js$" | wc -l)
+    a_jsname=$(ls -l /scripts | grep -oE "^-.*js$" | grep -oE "[^ ]*js$")
+    monkcoder
+    whyour
+    b_jsnum=$(ls -l /scripts | grep -oE "^-.*js$" | wc -l)
+    b_jsname=$(ls -l /scripts | grep -oE "^-.*js$" | grep -oE "[^ ]*js$")
+    #
+    diycron
+    #
+    info_more=$(echo $a_jsname  $b_jsname | tr " " "\n" | sort | uniq -c | grep -oE "1 .*$" | grep -oE "[^ ]*js$" | tr "\n" " ")
+    [[ "$a_jsnum" == "0" || "$a_jsnum" == "$b_jsnum" ]] || curl -sX POST "https://api.telegram.org/bot$TG_BOT_TOKEN/sendMessage" -d "chat_id=$TG_USER_ID&text=DIY脚本更新完成：$a_jsnum $b_jsnum $info_more" >/dev/null
+}
+
+main
